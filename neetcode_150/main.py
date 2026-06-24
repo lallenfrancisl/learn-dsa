6b77cc8a3398d69f65bfdc27fb7f83a5c796d071
Allen Francis <lallenfrancisl@gmail.com>
2026-06-24 12:09:51 +0530

    feat: solve two_sum

--- neetcode_150/main.py
+++ neetcode_150/main.py
@@ -17,6 +17,11 @@ INPUTS: dict[str, list[TestCase]] = {
         (("rat", "car"), False),
         (("aacc", "ccac"), False),
     ],
+    "1_two_sum.py": [
+        (([2, 7, 11, 15], 9), [0, 1]),
+        (([3, 2, 4], 6), [1, 2]),
+        (([3, 3], 6), [0, 1]),
+    ],
 }
 
 
