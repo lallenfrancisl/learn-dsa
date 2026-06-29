d99f5a968f80fe496956c6427311e6905caeb7e8
Allen Francis <lallenfrancisl@gmail.com>
2026-06-29 20:11:25 +0530

    solve invert binary tree

---
+++ neetcode_150/226_invert_binary_tree.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+from tree_node import TreeNode
+
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root is None:
+            return None
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        tmp = root.left
+        root.left = root.right
+        root.right = tmp
+
+        return root
---
+++ neetcode_150/tree_node.py
@@ -0,0 +1,26 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __repr__(self) -> str:
+        values = []
+        level = [self]
+
+        while any(level):
+            values.extend(node.val if node else None for node in level)
+
+            next_level = []
+            for node in level:
+                if node:
+                    next_level.extend([node.left, node.right])
+                else:
+                    next_level.extend([None, None])
+
+            while next_level and next_level[-1] is None:
+                next_level.pop()
+
+            level = next_level
+
+        return str(values)
