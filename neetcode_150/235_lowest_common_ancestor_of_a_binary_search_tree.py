b0fb58a295127ffaadac2eea491d5efe9aa2f4ce
Allen Francis <lallenfrancisl@gmail.com>
2026-06-30 09:23:11 +0530

    solve lowest_common_ancestor_of_a_binary_tree

---
+++ neetcode_150/235_lowest_common_ancestor_of_a_binary_search_tree.py
@@ -0,0 +1,18 @@
+from tree_node import TreeNode
+
+
+class Solution:
+    def lowestCommonAncestor(
+        self, root: TreeNode, p: TreeNode, q: TreeNode
+    ) -> TreeNode:
+        curr = root
+
+        while curr:
+            if p.val < curr.val and q.val < curr.val:
+                curr = curr.left
+            elif p.val > curr.val and q.val > curr.val:
+                curr = curr.right
+            else:
+                return curr
+
+        return TreeNode()
